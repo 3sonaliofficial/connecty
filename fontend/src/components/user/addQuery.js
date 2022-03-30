816ba019cd7a1dde50d4f8a114e284e257690e31
shivi <palsonali554@gmail.com>
2022-03-30 19:11:15 +0530

    ..

--- fontend/src/components/user/addQuery.js
+++ fontend/src/components/user/addQuery.js
@@ -3,16 +3,42 @@ import React from "react";
 const AddQuery = () => {
   return (
     <div className="container mt-5">
-
       <div className="card">
-          <img src="https://wallpaperaccess.com/full/3533193.png" className="card-img-top" />
-        <div className="card-body">
-          
+        <img
+          src="https://wallpaperaccess.com/full/3533193.png"
+          className="card-img-top"
+        />
+     
+
+  <div class="card-body">
+  <h2>Add new query</h2>
+  <div className="card"  >
+  <h1>Text Area</h1>
+
+  </div>
+  
+  
+  </div>
+</div>
+<header>
+<div className="container">
+<div class="row"></div>
+
+
+       
+        <div class="d-grid gap-2 d-md-flex justify-content-md-end">
+  <button class="btn btn-primary me-md-6" type="button"> button</button>
+  
+</div>
         </div>
-      </div>
+        
+        
+</header>
 
+        
+      
     </div>
-
-  )};
+  );
+};
 
 export default AddQuery;
