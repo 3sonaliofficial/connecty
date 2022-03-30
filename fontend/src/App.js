baa91acebbb41c041e08f5bc12993946cee08a96
m-triple-m <triplem656@gmail.com>
2022-03-30 19:31:57 +0530

    ..

--- fontend/src/App.js
+++ fontend/src/App.js
@@ -1,18 +1,23 @@
 import "./App.css";
-import { BrowserRouter, Navigate, NavLink, Route, Routes } from "react-router-dom";
+import {
+  BrowserRouter,
+  Navigate,
+  NavLink,
+  Route,
+  Routes,
+} from "react-router-dom";
 import Main from "./components/main";
 import Home from "./components/main/home";
 
 import AddVendor from "./components/admin/addVendor";
-import AddReview from "./components/admin/addReview";
 import Admin from "./components/admin";
 import AdminDashboard from "./components/admin/dashboard";
 import User from "./components/user";
 import Dashboard from "./components/user/dashboard";
 import AddQuery from "./components/user/addQuery";
-import Login from "./components/main/login"
-import Signup from "./components/main/signup"
-
+import Login from "./components/main/login";
+import Signup from "./components/main/signup";
+import AddReview from "./components/user/addReview";
 
 function App() {
   return (
@@ -24,20 +29,17 @@ function App() {
             <Route element={<Signup />} path="signup" />
             <Route element={<Login />} path="login" />
           </Route>
-          <Route element= {<User />} path="user">
+          <Route element={<User />} path="user">
             <Route element={<AddReview />} path="addreview" />
-            <Route element={<Dashboard />} path="dashboard">
-              <Route element={<AddQuery />} path="query" />
-            </Route>
+            <Route element={<Dashboard />} path="dashboard" />
+            <Route element={<AddQuery />} path="query" />
           </Route>
           <Route element={<Admin />} path="admin">
             <Route element={<AdminDashboard />} path="dashboard" />
 
             <Route element={<AddVendor />} path="addvendor" />
           </Route>
-          <Route  element={<Navigate to="/main/home" />} path="" />
-          
-          
+          <Route element={<Navigate to="/main/home" />} path="" />
         </Routes>
       </BrowserRouter>
     </div>
